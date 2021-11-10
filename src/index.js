363c76324070cf6b9e1f8ed2fa7a37d39cf7218e
codepanther <pezechigozirim@gmail.com>
2021-11-10 17:53:06 +0100

    Fetch leaderboard scores from the API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import './style.css';
 
+
 const submitButton = document.querySelector('.submit-btn');
+const refreshButton = document.querySelector('.refresh-btn');
 const nameInput = document.querySelector('input[name="name"]');
 const scoreInput = document.querySelector('input[name="score"]');
 const formContainer = document.querySelector('.score-form');
+const scoresContainer = document.querySelector('.scores-list');
 
 const gameId = '3L4QvyqAZFTD6eSge8xk';
+
+const displayScores = (scores) => {
+  scoresContainer.innerHTML = '';
+
+  scores.forEach((score, index) => {
+    scoresContainer.innerHTML += `<li class="score-item ${index % 2 ? 'ash' : ''}">
+      ${score.user}: ${score.score}
+    </li>`
+  })
+}
 
 const displayMessage = (msg) => {
   const span = document.createElement('span');
   span.innerHTML = msg;
+  span.style.display = 'block';
   formContainer.append(span);
 }
 
@@ -29,6 +43,14 @@ async function submitScore(name, score) {
   displayMessage(data.result);
 }
 
+const fetchScores = async function() {
+  const res = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
+  const data = await res.json();
+  
+  const scores = data.result;
+  displayScores(scores);
+}
+
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   const playerName = nameInput.value;
@@ -37,5 +59,6 @@ submitButton.addEventListener('click', (e) => {
   submitScore(playerName, playerScore);
 })
 
-
-
+refreshButton.addEventListener('click', () => {
+  fetchScores();
+})
